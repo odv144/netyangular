9ee7a1d691152e034a24cf561a2af04e26828684
Omar Dario Virili <omar.virili@gmail.com>
2022-12-24 11:35:59 -0300

    algunos ejercicios rama develop

---
+++ Ejercicios3y4/Ejercicios/ProyectoColleccion/Program.cs
@@ -0,0 +1,87 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#region Ejercicio 5
+/*
+string[,] matriz = new string[5,5];
+
+for(int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 5; j++)
+    {
+        if((i+j) % 2 == 0)
+        {
+            matriz[i,j] = "P";
+        }
+        else
+        {
+            matriz[i,j] = "I";
+        }
+    }
+}
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write(matriz[i, j]);
+        
+    }
+    Console.WriteLine("");
+}
+*/
+#endregion
+
+#region Ejercicio 6
+var randomNumber = new Random();
+int[,] temperatura = new int[5, 7];
+int con = 0;
+for(int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 7; j++)
+    {
+        if(i+j>0 && con<32)
+            temperatura[i, j] = randomNumber.Next(7, 39);
+
+        con++;
+    }
+}
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 7; j++)
+    {
+        Console.Write(temperatura[i, j]+"|"); 
+    }
+    Console.WriteLine("");
+}
+#endregion
