20f40ba78bc6dc53f39518003aa38f394444d67c
odv144 <omar.virili@gmail.com>
2022-12-27 19:41:25 -0300

    modificacion layout casa y agregado de paginas

--- Ejercicios3y4/Ejercicios/WebAppOmar/Controllers/HomeController.cs
+++ Ejercicios3y4/Ejercicios/WebAppOmar/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WebAppOmar.Controllers
             return View();
         }
 
+        public IActionResult Casa()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
