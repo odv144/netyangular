3a2c4f04a1e229302ae3cd541f7ba1006f3a163e
Omar Dario Virili <omar.virili@gmail.com>
2022-12-31 10:15:56 -0300

    .gitignore funcioanod en rama Taller

--- Ejercicios3y4/Ejercicios/WebApiMia/Program.cs
+++ Ejercicios3y4/Ejercicios/WebApiMia/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//nose si me esta tomando todo
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
